38442ab9c540c00d33270b1e38da9f179fc029fa
Elhamrababah <Elham.a.rababah@gmail.com>
2018-05-28 13:43:18 +0300

    fix app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,9 @@ import { HeaderComponent } from './header/header.component';
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemsComponent } from './recipes/recipe-items/recipe-items.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { ShoppingComponent } from './shopping/shopping.component';
-import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list//shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 
@@ -19,9 +17,8 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     HeaderComponent,
     RecipesComponent,
     RecipeListComponent,
-    RecipeItemsComponent,
+    RecipeItemComponent,
     RecipeDetailComponent,
-    ShoppingComponent,
     ShoppingListComponent,
     RecipeItemComponent,
     ShoppingEditComponent
