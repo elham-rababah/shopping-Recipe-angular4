f54cc8e1128d26ff00d25ffde431ff5ad9978c92
Elhamrababah <Elham.a.rababah@gmail.com>
2018-05-29 12:53:26 +0300

    show the recipe list with real data and fix the design issue

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 	recipes: Recipe[] = [
-		new Recipe ('Recipe name','Tary Tasty','https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2Frecipes%2Fck%2F1')
+		new Recipe ('Recipe name','Tary Tasty','https://is1-ssl.mzstatic.com/image/thumb/Purple128/v4/a1/c2/b6/a1c2b6e3-c5e5-b283-977b-9a0be775c0fa/mzl.mrmxmoym.jpg/643x0w.jpg'),
+		new Recipe ('Recipe name1','Tary Tasty1','http://img1.cookinglight.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2016/09/main/1101-couscous-chicken-ck.jpg?itok=7geW7sC_')
 	];
 
   constructor() { }
