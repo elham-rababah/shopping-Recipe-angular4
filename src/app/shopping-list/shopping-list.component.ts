d930f5e00641389c47d7a4f24cde6aedb15b01ff
Elhamrababah <Elham.a.rababah@gmail.com>
2018-05-29 14:19:25 +0300

    add ingrediants to shping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../shaerd/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+	ingredients: Ingredient[] = [
+		new Ingredient('Apple',5),
+		new Ingredient('Apple1',1),
+		new Ingredient('Apple2',2),
+	];
 
   constructor() { }
 
