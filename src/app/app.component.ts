6979b22ffda3f518d0986eb5c03465b0c9669c31
Elhamrababah <Elham.a.rababah@gmail.com>
2018-05-31 14:00:58 +0300

    add mock routeing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+	route:string = 'Recipes';
+	routeVlaue(route) {
+		this.route = route;
+	}
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter  } from '@angular/core';
 
 @Component({
 	selector: 'app-header',
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
+	@Output() routeVlaue = new EventEmitter<string>();
+
+	determineRouteValue(route){
+		this.routeVlaue.emit(route);
+
+	}
 }
