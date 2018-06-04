28f36090b53e285adc834768283a5051fc7aeada
Elhamrababah <Elham.a.rababah@gmail.com>
2018-06-04 12:32:19 +0300

    create services and use recipe service as data provider

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,8 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+	recipes: Recipe[] = [
+		new Recipe ('Recipe name','Tary Tasty','https://is1-ssl.mzstatic.com/image/thumb/Purple128/v4/a1/c2/b6/a1c2b6e3-c5e5-b283-977b-9a0be775c0fa/mzl.mrmxmoym.jpg/643x0w.jpg'),
+		new Recipe ('Recipe name1','Tary Tasty1','http://img1.cookinglight.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2016/09/main/1101-couscous-chicken-ck.jpg?itok=7geW7sC_')
+	];
+}
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { RecipeService } from './recipe.service';
 import { Recipe } from './recipe.model'
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  styleUrls: ['./recipes.component.css'],
+  providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
 	
 	recipe: Recipe;
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
   }
