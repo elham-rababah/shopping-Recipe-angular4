aabad1448116a5ca6a3c145fc4d3154d16a77adb
Elhamrababah <Elham.a.rababah@gmail.com>
2018-06-13 12:48:00 +0300

    signin via firebase SDK

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -15,4 +15,15 @@ export class AuthService {
 			console.log(error);
 		});
 	}
+
+	signinUser(email :string, password: string) {
+		firebase.auth()
+		.signInWithEmailAndPassword(email, password)
+		.then((response)=>{
+			console.log(response);
+		})
+		.catch((error)=>{
+			console.log(error);
+		});
+	}
 }
