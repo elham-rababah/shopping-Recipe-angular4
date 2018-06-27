828beabbc712258fd7f648d46a78b4dc9f06f1b2
Elhamrababah <Elham.a.rababah@gmail.com>
2018-06-27 10:29:55 +0300

    fix add ingredient from recipe detail

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit,Input } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+
 import { Recipe } from '../recipe.model'
 import { ShoppingListService } from '../../shopping-list/shopping-list.service';
 import { RecipeService } from '../recipe.service';
+import * as FromShoppingList from '../../shopping-list/ngrx-store/shopping-list.reducers';
+import * as ShoppingListActions from '../../shopping-list/ngrx-store/shopping-list.actions'; 
 
 @Component({
   selector: 'app-recipe-detail',
@@ -18,7 +22,8 @@ export class RecipeDetailComponent implements OnInit {
     private shoppingListService: ShoppingListService,
     private route: ActivatedRoute,
     private recipeService: RecipeService,
-    private router: Router
+    private router: Router,
+    private store: Store<FromShoppingList.AppState>,
     ) { }
 
   ngOnInit() {
@@ -31,7 +36,7 @@ export class RecipeDetailComponent implements OnInit {
   addToShopping(){
   	this.recipe.ingredients
   	.map((ingredient)=>{
-  		//this.shoppingListService.addIngrediant(ingredient);
+        this.store.dispatch(new ShoppingListActions.AddIngredient(ingredient));
   	})
   }
 
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,15 +1,27 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject'
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Store } from '@ngrx/store';
 import { Ingredient } from '../shaerd/ingredient.model';
 import { AuthService } from '../auth/auth.service';
+import * as FromShoppingList from './ngrx-store/shopping-list.reducers';
 
 
 @Injectable()
 
 export class ShoppingListService {
+	ingredients = [];
+	constructor(
+		private httpClient: HttpClient, 
+		private authService: AuthService,
+		private store: Store<FromShoppingList.AppState>
+		){
+			this.store.select('shoppingList').subscribe((data)=>{
+				this.ingredients = data.ingredients;
+			})
+		}
+	
 
-	constructor(private httpClient: HttpClient, private authService: AuthService){}
 	saveIngredientsData() {
 		let tokenId = this.authService.getIdToken();
 		return this.httpClient
